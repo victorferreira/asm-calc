58599289bd8e6a3e202bb2830289f41b9ac3317d
Victor Ferreira <victor.araujo.ferreira@gmail.com>
2016-09-06 11:03:39 -0300

    add more token identifiers

--- src/calc.c
+++ src/calc.c
@@ -2,11 +2,47 @@
 #include <stdlib.h>
 
 int newLine = 0;
+int attribution = 0;
+int addition = 0;
+int subtraction = 0;
+int multiplication = 0;
+int division = 0;
+int exponentiation = 0;
 
-void checkCharcter(char character) {
-  if (character == '\n')
-    newLine++;
-  putchar(character);
+void printTokens() {
+  printf("%d new lines\n", newLine);
+  printf("%d attribution tokens\n", attribution);
+  printf("%d addition tokens\n", addition);
+  printf("%d subtraction tokens\n", subtraction);
+  printf("%d multiplication tokens\n", multiplication);
+  printf("%d division tokens\n", division);
+  printf("%d exponentiation tokens\n", exponentiation);
+}
+
+void checkCharacter(char character) {
+  switch (character) {
+    case '\n':
+      newLine++;
+      break;
+    case '=':
+      attribution++;
+      break;
+    case '+':
+      addition++;
+      break;
+    case '-':
+      subtraction++;
+      break;
+    case '*':
+      multiplication++;
+      break;
+    case '/':
+      division++;
+      break;
+    case '^':
+      exponentiation++;
+      break;
+  }
 }
 
 int readFile(FILE* fp) {
@@ -20,10 +56,11 @@ int readFile(FILE* fp) {
   int c; // note: int, not char, required to handle EOF
   while ((c = fgetc(fp)) != EOF) { // standard C I/O file reading loop
     // putchar(c);
-    checkCharcter(c);
+    checkCharacter(c);
   }
 
-  printf("%d New lines\n", newLine);
+  // printf("%d New lines\n", newLine);
+  printTokens();
 
   if (ferror(fp))
   puts("I/O error when reading");
